180ddbc5de7e264c3fb8ed3e2f14000e150900c0
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-01-21 02:27:41 +0530

    Strings in java

---
+++ src/Strings/ReverseStringbuilder.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class ReverseStringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sc=new StringBuilder("Shiva");
+
+        for(int i =0;i<sc.length()/2;i++){
+            int front=i;
+            int backChar=sc.length() -1 -i; //5-1-0=4;
+
+            
+
+
+
+
+        }
+    }
+}
--- src/Strings/builder.java
+++ src/Strings/builder.java
@@ -9,23 +9,28 @@ public class builder {
         sc.setCharAt(0,'p');
         System.out.println(sc);
 
-        sc.insert(0,'S');
+        // sc.insert(0,'S');
+        // System.out.println(sc);
+
+        // //write from spony to sponny
+        // sc.insert(3, 'n');
+        // System.out.println(sc);
+
+        // //delte the extra e from Spoeny
+        // sc.delete(3,4);
+
+        // //delete the extra n from Sponny
+        // sc.delete(3,3);
+        // System.out.println(sc);
+
+        // //Make from Spony to Spy
+        // sc.delete(2,4);
+        // System.out.println(sc); 
+
+        sc.insert(4, "Tail");
         System.out.println(sc);
 
-        //write from spony to sponny
-        sc.insert(3, 'n');
+        sc.delete(4, sc.length());
         System.out.println(sc);
-
-        //delte the extra e from Spoeny
-        sc.delete(3,4);
-
-        //delete the extra n from Sponny
-        sc.delete(3,3);
-        System.out.println(sc);
-
-        //Make from Spony to Spy
-        sc.delete(2,4);
-        System.out.println(sc); 
-
     }
 }
