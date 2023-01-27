f05b2a0ed2680b0d9fe120f2be7ec1a1cfcab132
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-01-27 19:24:49 +0530

    27-01-2023

---
+++ src/Arrays/ArrayException.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ArrayException {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        for(int i=0;i<=arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/Arrays/CopyArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {  
+        //declaring a source array  
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',  
+                'i', 'n', 'a', 't', 'e', 'd' };  
+        //declaring a destination array  
+        char[] copyTo = new char[7];  
+        //copying array using System.arraycopy() method  
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);  
+        //printing the destination array  
+        System.out.println(String.valueOf(copyTo));  
+    }  
+}
