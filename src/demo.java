6f1190d19d16fb8148bb8d9494925debddf0bbfd
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-02 03:45:43 +0530

    .

--- src/demo.java
+++ src/demo.java
@@ -1,19 +1,9 @@
 public class demo{
     public static void main(String[] args) {
-        int i;
-        for(i=3;i<=300;i+=3);
-        System.out.println(i);
-   
-        // int x=2;
-        // if(x--,--x,x){
-
-        //     System.out.println("tcs");
-        //     else{
-        //         System.out.println("hello");
-        //     }
-
-        // }
-   
+       for(int i =0;i<3;i++){
+        System.out.println(i+ " ");
+       }
+       System.out.println(i+ " ");
    
     }
 }
