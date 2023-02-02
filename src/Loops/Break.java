9a8ad2f1ea7a637662b080406cfc40ccbae48d03
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-03 00:39:05 +0530

    03-02-2023

---
+++ src/Loops/Break.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class Break {
+    public static void main(String[] args) {
+        int n=5;
+        //int i=1;
+        for(int i=1;i<=n;i++){
+            if(i==4){
+             break;
+        }
+
+        System.out.println(i);
+    }
+    }
+}
+ 
