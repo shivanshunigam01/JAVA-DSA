c2e085027b1788b6add36a27c9af9028d3d29c48
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-15 15:52:08 +0530

    Update on 15-02-2023

---
+++ src/GFG_Problems/Number_inverted.java
@@ -0,0 +1,7 @@
+package GFG_Problems;
+
+public class Number_inverted {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/GFG_Problems/Right_Trianlge.java
@@ -0,0 +1,17 @@
+package GFG_Problems;
+
+public class Right_Trianlge {
+    public static void main(String[] args) {
+        System.out.println("Let's print the right angle triangle");
+        
+        int n=5;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+        
+            System.out.println("");
+        
+        }
+    }
+}
