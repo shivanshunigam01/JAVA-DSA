a4921fc586436719a44cd7e6cfa4e9b27305c340
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-15 16:03:19 +0530

    15-02-2023

--- src/GFG_Problems/Number_inverted.java
+++ src/GFG_Problems/Number_inverted.java
@@ -2,6 +2,17 @@ package GFG_Problems;
 
 public class Number_inverted {
     public static void main(String[] args) {
-        
+     System.out.println("Opposite pyramid in java");   
+     int n=4;
+     for(int i=1;i<=n;i++){
+        for(int k=1;k<=4-i;k++){
+            System.out.print(" ");
+        }
+     
+        for(int j=1;j>i;j-- ){
+            System.out.println(j);
+        }
+    System.out.println("");
+    }
     }
 }
