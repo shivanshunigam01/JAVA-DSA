000e0a032361be46af81ab103d241907274c124f
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-15 15:09:41 +0530

    15-02-2023

---
+++ src/GFG_Problems/Perimeter_of_triangle.java
@@ -0,0 +1,46 @@
+package GFG_Problems;
+import java.util.Scanner;
+public class Perimeter_of_triangle {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the length:");
+    int length=sc.nextInt();
+ 
+    System.out.print("Enter the breadth:");
+    int breadth=sc.nextInt();
+    
+    int Perimeter_of_triangle;
+    Perimeter_of_triangle=2*(length+breadth);
+    System.out.println("Perimeter of triangle:"+Perimeter_of_triangle);
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
