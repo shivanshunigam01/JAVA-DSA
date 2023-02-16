8939a49aba0acfa0ed107d7aaa8e45ebaa93a938
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 20:01:23 +0530

    16-02-2023

---
+++ src/GFG_Problems/Swap_three_numbers.java
@@ -0,0 +1,29 @@
+package GFG_Problems;
+
+import java.util.Scanner;
+
+public class Swap_three_numbers {
+
+  
+        
+ public static void main(String[] args) {
+        int n1,n2;
+    
+        Scanner sc=new Scanner(System.in);
+         n1=sc.nextInt();
+         n2=sc.nextInt();
+        //  n3=sc.nextInt();
+System.out.println("Let's print the swap man!");
+int temp;
+temp=n1;
+n1=n2;
+n2=temp;
+System.out.println(n1);
+System.out.println(n2);
+
+
+    }
+
+
+
+}
