89bb6fdd9e91025d9e25334225668fc2e6fb8c67
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 19:03:56 +0530

    16-02-2023

---
+++ src/GFG_Problems/ASCII_value.java
@@ -0,0 +1,21 @@
+package GFG_Problems;
+
+import java.util.Scanner;
+
+public class ASCII_value {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the char:");
+        char ch=sc.next().charAt(0);
+
+        int a=ch;   
+        System.out.println("The value of ASCII Value is:"+a);
+
+
+    }
+
+
+
+
+}
