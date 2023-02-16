5d7329f39909bfcfbfc984828440e105e97bb43f
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 19:04:55 +0530

    16-02-2023

---
+++ src/GFG_Problems/Average_of_sub.java
@@ -0,0 +1,30 @@
+package GFG_Problems;
+
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Average_of_sub {
+    public static void main(String[] args) {
+     
+    Scanner sc=new Scanner(System.in);
+    int a,b,c,d,e;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    d=sc.nextInt();
+    e=sc.nextInt();
+    
+int sum=a+b+c+d+e;
+System.out.println("Sum of marks will be:"+sum);
+int average=sum/5.2;
+System.out.println(average);
+
+
+
+
+
+    }
+
+    
+}
