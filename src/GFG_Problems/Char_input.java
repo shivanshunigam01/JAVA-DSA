6436d02bfe743060710157b0e3d0190cf6b4ce1a
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 19:03:12 +0530

    16-02-2023

---
+++ src/GFG_Problems/Char_input.java
@@ -0,0 +1,13 @@
+package GFG_Problems;
+
+import java.util.Scanner;
+
+public class Char_input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the char value:");
+        char hey=sc.next().charAt(0);
+        System.out.println(hey);
+
+    }
+}
