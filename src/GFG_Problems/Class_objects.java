094dac15af9b6bd20f88bd0876a97dc004bb63c3
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 18:36:03 +0530

    16-02-2023

---
+++ src/GFG_Problems/Class_objects.java
@@ -0,0 +1,17 @@
+package GFG_Problems;
+
+public class Class_objects {
+
+    String name;
+    int age, salary;
+
+    public static void main(String[] args) {
+        Class_objects ob1 = new Class_objects();
+        ob1.name="Shiva";
+        ob1.age=23;
+        ob1.salary=5000000;
+        System.out.println("Let's print "+ob1.name +" "+ob1.age+" "+ob1.salary);
+
+
+    }
+}
