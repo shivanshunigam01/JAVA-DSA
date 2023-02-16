2ba09f6c2ca548266ef9ab6cd126ef3847ab92ac
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-16 19:03:37 +0530

    16-02-2023

---
+++ src/GFG_Problems/A_to_z.java
@@ -0,0 +1,9 @@
+package GFG_Problems;
+
+public class A_to_z {
+    public static void main(String[] args) {
+        for( char i='A';i<='Z';i++){
+            System.out.print(" "+i);
+        }
+    }
+}
