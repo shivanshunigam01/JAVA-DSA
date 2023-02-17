4f2e6209a0f49cc7648164b56881321d9661cf99
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-17 12:10:39 +0530

    17-02-2023

---
+++ src/GFG_Problems/Sum_of_digits.java
@@ -0,0 +1,19 @@
+package GFG_Problems;
+
+import java.util.Scanner;
+
+public class Sum_of_digits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int reverse,sum=0;
+
+        while(n>0){
+            reverse=n%10;
+            n=n/10;
+            sum=reverse+sum;
+        }
+        System.out.println(sum);
+    }
+}
