52d742e3be3033c572c1bda3f49fa176fadedbd4
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-20 18:55:48 +0530

    20-02-2023

---
+++ src/GFG_Problems/MyClass3.java
@@ -0,0 +1,48 @@
+package GFG_Problems;
+
+// public class Demo {
+//     public static void main(String[] args) {
+ 
+//            int x = 7;
+// int y = x;
+// x = 2;
+// System.out.println(x + ", " + y);
+//     }
+ 
+// }
+
+public class MyClass3
+{
+  private int a;
+
+  public MyClass3(int first)
+  {
+    this.a = first;
+  }
+}
+
+public class hey class MyClass3
+{
+  private int a;
+
+  public MyClass3(int first)
+  {
+    this.a = first;
+  }
+}
+
+public class MyClassTester
+{
+  public static void main(String[] args)
+  {
+    MyClass3 c1 = new MyClass3(30);
+    System.out.println(c1.a);
+  }
+}
+{
+  public static void main(String[] args)
+  {
+    MyClass3 c1 = new MyClass3(30);
+    System.out.println(c1.a);
+  }
+}
